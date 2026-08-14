1924b20047d4914ea2bfc0a43930b22836bc48f8
Serious Hoax <serioushoax@gmail.com>
2026-08-15 01:06:03 +0600

    Update gitlab mirror link

--- update_gateway.py
+++ update_gateway.py
@@ -60,7 +60,7 @@ blocklists: List[Dict[str, str]] = [
     {
         "name": "Hagezi Pro++",
         "url": "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus-onlydomains.txt",
-        "backup_url1": "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/wildcard/pro.plus-onlydomains.txt?ref_type=heads",
+        "backup_url1": "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/wildcard/pro.plus-onlydomains.txt",
         "backup_url2": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus-onlydomains.txt",
         "priority": 10000
     }
